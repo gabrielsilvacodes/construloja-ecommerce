8b19bfe613f0d535257db675227340d3b42ab83e
gabrielsilvacodes <gabrielsilvadeveloper@outlook.com>
2025-06-06 19:16:52 -0300

    ajustes

--- src/app/produtos/[id]/page.tsx
+++ src/app/produtos/[id]/page.tsx
@@ -1,9 +1,18 @@
+// src/app/produtos/[id]/page.tsx
 import ProdutoClient from "@/components/product/ProdutoClient";
+import type { Metadata } from "next";
 
-interface PageProps {
-  params: { id: string };
-}
+export const metadata: Metadata = {
+  title: "Detalhes do Produto",
+  description: "Informações sobre o produto selecionado",
+};
 
-export default function Page({ params }: PageProps) {
-  return <ProdutoClient id={params.id} />;
+export default async function ProdutoPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
+  return <ProdutoClient id={id} />;
 }
--- src/lib/firebaseConfig.ts
+++ src/lib/firebaseConfig.ts
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDtK7GTc8bUucoIuSXSBZeoUcrzKbbEQlo",
-  authDomain: "construloja-ddb06.firebaseapp.com",
-  projectId: "construloja-ddb06",
-  storageBucket: "construloja-ddb06.firebasestorage.app",
-  messagingSenderId: "262018103194",
-  appId: "1:262018103194:web:ff8f3437e81df2fbe4ec23",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
 
 const app = initializeApp(firebaseConfig);
